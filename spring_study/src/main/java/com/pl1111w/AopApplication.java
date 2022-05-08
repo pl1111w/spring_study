6fa84d7efefc90626afabac61a46db77e3bf366d
xiaobo wei <1362596047@qq.com>
2022-05-08 15:21:36 +0800

    admin

--- spring_study/src/main/java/com/pl1111w/AopApplication.java
+++ spring_study/src/main/java/com/pl1111w/AopApplication.java
@@ -1,11 +1,13 @@
 package com.pl1111w;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
+@EnableAdminServer
 public class AopApplication {
 
     public static void main(String[] args) {
